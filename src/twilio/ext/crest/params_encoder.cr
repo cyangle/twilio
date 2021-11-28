55bdbfbe958cac936e2dfe9bdf3f8a93359fe5e8
Chao Yang <cyang05@outlook.com>
2021-11-28 14:39:35 -0600

    Refactor Crest::ParamsEncoder extension

--- src/twilio/ext/crest/params_encoder.cr
+++ src/twilio/ext/crest/params_encoder.cr
@@ -6,14 +6,14 @@ module Crest
     #
     # ```
     # Crest::ParamsEncoder.flatten_params({:key1 => {:arr => ["1", "2", "3"]}})
-    # # => [{"key1[arr]", "1"}, {"key1[arr]", "2"}, {"key1[arr]", "3"}]
+    # # => [{"key1", "1"}, {"key1", "2"}, {"key1", "3"}]
     # ```
     def flatten_params(object : Array, parent_key = nil) : Array(Tuple(String, ParamsValue))
       object.reduce([] of Tuple(String, ParamsValue)) do |memo, item|
         k = :""
         v = item
 
-        processed_key = parent_key ? "#{parent_key}" : k.to_s
+        processed_key = parent_key ? parent_key : k.to_s
         memo << {processed_key, v}
 
         memo
